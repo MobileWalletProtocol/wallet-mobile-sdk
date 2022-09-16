53aa7ec7ffbc747f87acfc9c9dce9a6d5073d66b
Jungho Bang <jungho.bang@coinbase.com>
2022-09-15 17:31:03 -0700

    fix build issue

--- flutter/ios/Classes/SwiftCoinbaseWalletSdkFlutterPlugin.swift
+++ flutter/ios/Classes/SwiftCoinbaseWalletSdkFlutterPlugin.swift
@@ -135,10 +135,10 @@ public class SwiftCoinbaseWalletSdkFlutterPlugin: NSObject, FlutterPlugin {
                         ]
                     }
                     switch it {
-                    case .result(let value):
+                    case .success(let value):
                         response["result"] = value
-                    case .error(let code, let message):
-                        response["error"] = ["code": code, "message": message]
+                    case .failure(let error):
+                        response["error"] = ["code": error.code, "message": error.message]
                     }
                     
                     toFlutter.append(response)
