0ab7d83a145431a86babd5935078688af1aaf0d5
Jungho Bang <jungho.bang@coinbase.com>
2022-11-01 22:54:49 +0900

    config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,18 +6,19 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Mobile Wallet Protocol',
+  tagline: 'Mobile Wallet Protocol',
+  url: 'https://coinbase.github.io/wallet-mobile-sdk',
+  baseUrl: '/wallet-mobile-sdk/',
+  deploymentBranch: 'doc',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'coinbase', // Usually your GitHub org/user name.
+  projectName: 'wallet-mobile-sdk', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
