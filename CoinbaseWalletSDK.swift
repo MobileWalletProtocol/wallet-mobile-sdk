6f6413e881e82ea3a9b1b9777b6f95be22dbf883
Jungho Bang <jungho.bang@coinbase.com>
2022-11-18 16:03:26 -0800

    legacy support

---
+++ CoinbaseWalletSDK.swift
@@ -0,0 +1,13 @@
+//
+//  CoinbaseWalletSDK.swift
+//  MobileWalletProtocol
+//
+//  Created by Jungho Bang on 11/18/22.
+//
+
+import Foundation
+
+@available(*, deprecated, message: "Use MWPClient instead")
+class CoinbaseWalletSDK {
+    
+}
