203fba7a709825a263b179b35004c902d6a0d3f2
Jungho Bang <jungho.bang@coinbase.com>
2022-11-23 11:10:01 -0800

    iconUrl

--- ios/CoinbaseWalletSDK/Client/Model/Wallet.swift
+++ ios/CoinbaseWalletSDK/Client/Model/Wallet.swift
@@ -10,8 +10,8 @@ import UIKit
 
 public struct Wallet {
     public let name: String
-    public let url: URL
     public let iconUrl: URL
+    public let url: URL
     public let mwpScheme: URL
     public let appStoreUrl: URL
     
@@ -24,16 +24,16 @@ public struct Wallet {
 extension Wallet {
     public static let coinbaseWallet = Wallet(
         name: "Coinbase Wallet",
-        url: URL(string: "https://wallet.coinbase.com/wsegue")!,
         iconUrl: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Purple122/v4/bd/2b/dc/bd2bdcac-44ff-0707-3ec1-fde5014a91a1/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/100x100bb.jpg")!,
+        url: URL(string: "https://wallet.coinbase.com/wsegue")!,
         mwpScheme: URL(string: "cbwallet://")!,
         appStoreUrl: URL(string: "https://apps.apple.com/app/id1278383455")!
     )
     
     public static let coinbaseRetail = Wallet(
         name: "Coinbase",
-        url: URL(string: "https://coinbase.com/wsegue")!,
         iconUrl: URL(string: "https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/4f/75/a6/4f75a665-0328-7bbc-f7ac-cee4e17c50c6/AppIcon-0-1x_U007emarketing-0-10-0-85-220.png/100x100bb.jpg")!,
+        url: URL(string: "https://coinbase.com/wsegue")!,
         mwpScheme: URL(string: "coinbase+mwp://")!,
         appStoreUrl: URL(string: "https://apps.apple.com/app/id886427730")!
     )
