3b2186bfe8ed7bd4b6f71450851e0b37d3ab8bee
Vishnu Madhusoodanan <vishnu.madhusoodanan@coinbase.com>
2022-12-05 17:37:24 -0600

    Fix how example client app is setup

--- react-native/client/example/babel.config.js
+++ react-native/client/example/babel.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+const path = require('path');
+const pak = require('../package.json');
+
+const libraryIndex = path.join(__dirname, '..', 'src', 'CoinbaseWalletSDK.ts');
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            // For development, we want to alias the library to the source
+            [pak.name]: libraryIndex,
+          },
+        },
+      ],
+    ],
+  };
 };
