ce1b9cc56f7973a1fe12f0eb26e6371efd3e663d
Jungho Bang <jungho.bang@coinbase.com>
2024-02-26 18:18:19 -0800

    CoinbaseWalletSDK.getCoinbaseWalletMWPVersion

--- ios/example/SampleClient/ViewController.swift
+++ ios/example/SampleClient/ViewController.swift
@@ -57,7 +57,7 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         isCBWalletInstalledLabel.text = "\(CoinbaseWalletSDK.isCoinbaseWalletInstalled())"
-        self.log("Supported MWP Version: \(CoinbaseWalletSDK.getCoinbaseWalletMWPVersion() ?? "none")")
+        self.log("Available MWP Version: \(CoinbaseWalletSDK.getCoinbaseWalletMWPVersion() ?? "none")")
         updateSessionStatus()
     }
     
