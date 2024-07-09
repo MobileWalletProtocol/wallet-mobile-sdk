773d8db6734e09fa6dc83620f27087f02544d49a
Alfreedom <alfredo@walletconnect.com>
2024-07-09 18:27:33 +0200

    Update flutter/android/src/main/kotlin/com/coinbase/flutter/wallet_sdk/CoinbaseWalletSdkFlutterPlugin.kt
    Co-authored-by: vishnu <vismad@proton.me>

--- flutter/android/src/main/kotlin/com/coinbase/flutter/wallet_sdk/CoinbaseWalletSdkFlutterPlugin.kt
+++ flutter/android/src/main/kotlin/com/coinbase/flutter/wallet_sdk/CoinbaseWalletSdkFlutterPlugin.kt
@@ -3,7 +3,6 @@ package com.coinbase.flutter.wallet_sdk
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import androidx.annotation.NonNull
 import com.coinbase.android.nativesdk.CoinbaseWalletSDK
 import com.coinbase.android.nativesdk.message.request.Account
